cfe3c7eadafbfbc1609bd7b70cbd710304a582f7
hoby <m1ch3ng@gmail.com>
2024-02-04 16:19:00 +0800

    adjust indent

--- src/main/java/com/qingfeng/blog/BlogApplication.java
+++ src/main/java/com/qingfeng/blog/BlogApplication.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class BlogApplication {
 
-
     public static void main(final String[] args) {
         SpringApplication.run(BlogApplication.class, args);
     }
